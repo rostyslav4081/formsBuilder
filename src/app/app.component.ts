d62b091ce0736c801ac55b4a5bc4b7f4e7536f31
Jakub Navrátil <navrc.kuba@gmail.com>
2024-04-17 13:01:47 +0200

    Fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -34,25 +34,25 @@ export class AppComponent {
   renderers = [
     ...angularMaterialRenderers,
     ...angularPrimeNgRenderers,
-
-
-
-
-
   ];
   uischema = uischemaAsset;
   schema = schemaAsset;
 
-  i18n = { locale: 'de-DE' };
+  i18n = { locale: 'cz-CZ' };
   dateAdapter;
   ajv = createAjv({
     schemaId: 'id',
     allErrors: true
   });
   constructor(dateAdapter: DateAdapter<Date>) {
+
+    // custom file format
+    this.ajv.addFormat('file', (data) => true);
+
     this.ajv.addFormat('time', '^([0-1][0-9]|2[0-3]):[0-5][0-9]$');
     this.dateAdapter = dateAdapter;
     dateAdapter.setLocale(this.i18n.locale);
+
     this.ajv.addFormat('tel', maybePhoneNumber => {
       try {
         parsePhoneNumber(maybePhoneNumber, 'CZ');
