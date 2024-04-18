e39d6a51964585e9b34fa4b053f40e72c5fd73ba
Rostyslav Skovera <skovera@animatec.cz>
2024-04-18 16:16:30 +0200

    patterns

--- src/app/primengrenders/controls/date.render.ts
+++ src/app/primengrenders/controls/date.render.ts
@@ -6,18 +6,19 @@ import { JsonFormsAngularService, JsonFormsControl } from '@jsonforms/angular';
 @Component({
   selector: 'DateControlRenderer',
   template: `
-    <div [ngStyle]="{ display: hidden ? 'none' : '' }" class="input-control">
+    <div [ngStyle]="{ display: hidden ? 'none' : '' }" class="input-control" >
       <label>{{ label }}</label>
 
 
       <p-calendar
         [formControl]="form"
-        (input)="onChange($event)"
         [id]="id"
         type="text"
         (focus)="focused = true"
         (focusout)="focused = false"
         styleClass="width-fix"
+        [dateFormat]="'yy-mm-dd'"
+        (onSelect)="onSelectDate($event)"
       >
 
       </p-calendar>
@@ -41,16 +42,30 @@ import { JsonFormsAngularService, JsonFormsControl } from '@jsonforms/angular';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DatePrimeNgControlRenderer extends JsonFormsControl {
+
+  override data: string | undefined;
   focused = false;
 
   constructor(jsonformsService: JsonFormsAngularService) {
     super(jsonformsService);
   }
 
-  override getEventValue = (event: any) => event.value.toISOString().substr(0, 10);
+
+  onSelectDate(event: any) {
+    const selectedDate: Date = event;
+    console.log('Selected date:', selectedDate);
+
+    const formattedDate: string = selectedDate.toISOString().split('T')[0];
+    this.form.setValue(new Date(formattedDate));
+    this.data = formattedDate;
+
+    this.onChange(event);
+  }
+
+  override getEventValue = () => this.data;
 }
 
-export const DatePrimeNgControlRendererTester: RankedTester = rankWith(
+  export const DatePrimeNgControlRendererTester: RankedTester = rankWith(
   3,
   isDateControl
 );
